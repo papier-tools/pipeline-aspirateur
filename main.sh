58b0390a0b433623b819616d519286d3382950a3
PapierPain <gourves.seven@gmail.com>
2022-05-06 01:00:12 +0200

    update

---
+++ main.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -x
+
+# Authors : Steven GOURVES
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#! MAIN:
+#!   Clear artifacts and pipelines in all projects.
+#!
+#! USAGE:
+#!   ./main.sh <job-token>
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+JOB_TOKEN=$1
+
+echo "Get all projects..."
+PROJECTS=$(curl --header "PRIVATE-TOKEN: ${JOB_TOKEN}" "$GITLAB_API/projects?per_page=500")
+
+echo "Get the project ids..."
+IFS=$'\n'
+PROJECT_IDS=($(echo "$PROJECTS" | jq -r '.[] | .id'))
+
+for i in "${!PROJECT_IDS[@]}"; do
+    functions/clear_pipelines "${JOB_TOKEN}" "${PROJECT_IDS[i]}" 5
+    functions/clear_artifacts "${JOB_TOKEN}" "${PROJECT_IDS[i]}"
+done
