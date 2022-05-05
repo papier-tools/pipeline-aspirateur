cf44633f69ce79a6cdbee9b8cefe3dea9afecba7
PapierPain <gourves.seven@gmail.com>
2022-05-06 01:06:55 +0200

    update

--- main.sh
+++ main.sh
@@ -12,19 +12,16 @@ set -x
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 JOB_TOKEN=$1
-RETURN_CODE=1
+GITLAB_API="https://gitlab.papierpain.fr/api/v4"
 
 echo "Get all projects..."
-PROJECTS=$(curl --header "PRIVATE-TOKEN: ${JOB_TOKEN}" "$GITLAB_API/projects?per_page=500")
+PROJECTS=$(curl --header "PRIVATE-TOKEN: ${JOB_TOKEN}" "${GITLAB_API}/projects?per_page=500")
 
 echo "Get the project ids..."
 IFS=$'\n'
-PROJECT_IDS=($(echo "$PROJECTS" | jq -r '.[] | .id'))
+PROJECT_IDS=($(echo "${PROJECTS}" | jq -r '.[] | .id'))
 
 for i in "${!PROJECT_IDS[@]}"; do
     functions/clear_pipelines "${JOB_TOKEN}" "${PROJECT_IDS[i]}" 5
     functions/clear_artifacts "${JOB_TOKEN}" "${PROJECT_IDS[i]}"
 done
-
-RETURN_CODE=0
-exit $RETURN_CODE
